5aa78b7fb2d7c06a74186c172e87831c3ef3db17
Matheus Figueredo <matheuslaurindo2017@gmail.com>
2023-09-17 23:37:00 -0300

    Criando pages para listagem e criacao

--- OnionArchitecture.WebSite/my-app/src/App.tsx
+++ OnionArchitecture.WebSite/my-app/src/App.tsx
@@ -1,51 +1,9 @@
-import { useEffect, useState } from "react";
-import Button from "./components/Button";
-import Table from "./components/Table";
-import TitleGrid from "./components/TitleGrid";
-import { BookResponseListDto } from "./dtos/bookDTO";
-import axios from "axios";
+import Listagem from "./pages/Listagem";
 
 function App() {
-  const url = "https://localhost:7025/api/Book/";
-  const [data, setData] = useState<BookResponseListDto[]>([
-    {
-      id: 0,
-      name: "",
-      description: "",
-      writer: "",
-      insertDate: "",
-    },
-  ]);
-
-  const getAll = async () => {
-    await axios
-      .get(url + "GetAllBooks")
-      .then((resp) => {
-        setData(resp.data);
-      })
-      .catch((error) => {
-        alert(error);
-      });
-  };
-
-  useEffect(() => {
-    getAll();
-  }, []);
-
   return (
     <div className="container mx-auto p-10 h-screen text-white bg-zinc-800">
-      <div className="grid grid-rows-1">
-        <div className="grid grid-cols-1">
-          <TitleGrid
-            title="BOOKS"
-            button={<Button text="Add new Book" color="zinc-700" />}
-          />
-          <Table
-            columns={["Name", "Description", "Writer", "Insert Date"]}
-            list={data}
-          />
-        </div>
-      </div>
+      <Listagem />
     </div>
   );
 }
